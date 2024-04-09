c874cac5f61a012080f609fc8faeeb30e6251528
Johnson <johnsonnjichi@gmail.com>
2024-04-09 17:12:56 +0300

    Components

---
+++ src/components/Input.js
@@ -0,0 +1,11 @@
+const Input = () => {
+
+    return (
+        <section>
+
+
+        </section>
+    )
+}
+
+export default Input
---
+++ src/components/Square.js
@@ -0,0 +1,10 @@
+const Square = () => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Square;
